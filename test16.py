e28b6bf48525cd3c085f6c3dbe12bbd6f4f0bc8c
tay7799586 <tay7799586@gmail.com>
2022-07-06 16:37:35 +0800

    0706

--- test16.py
+++ test16.py
@@ -1,6 +1,54 @@
+# import sqlite3
+# import MySQLdb
+
+# mysql_conn=MySQLdb.connect(
+#     host="127.0.0.1", #ip
+#     port= 3306, #appserv 
+#     suser= 'root',
+#     passwd= "Aa890907",
+#     db= 'nkustnews'
+# )
+# cursor = mysql_conn.cursor()
+# cursor.execute("SET  NAMES 'utf8'" )
+# cursor.execute("SET CHARACTER SET utf8")
+# db = sqlite3.connect("nkustnews.db")
+# sql = "select * from news;"
+# rows = db.execute(sql)
+# for row in rows:
+#     try:
+#         my_sql = "insert into news (title, content, date, url) values('{}','{}','{}','{}');".format(
+#             row[1], row[2], row[3], row[4]
+#         )
+#         cursor.execute(my_sql)
+#         mysql_conn.commit()
+#     except:
+#         pass
+# db.close()
+# mysql_conn.close()
 import sqlite3
-db= sqlite3.connect("nkustnews.db")
-sql="select * from news ;"
-rows=db.execute(sql)
+import MySQLdb
+
+mysql_conn = MySQLdb.connect(
+    host = "127.0.0.1",
+    port = 3306,
+    user = 'root',
+    passwd = 'Aa890907',
+    db = 'nkustnews'
+)
+cursor = mysql_conn.cursor()
+cursor.execute("SET NAMES 'utf8'")
+cursor.execute("SET CHARACTER SET utf8")
+db = sqlite3.connect("nkustnews.db")
+sql = "select * from news;"
+rows = db.execute(sql)
 for row in rows:
-    print(row[1])
+    try:
+        my_sql = "insert into news (title, content, date, url) values('{}','{}','{}','{}');".format(
+            row[1], row[2], row[3], row[4]
+        )
+        cursor.execute(my_sql)
+        mysql_conn.commit()
+    except:
+        pass
+db.close()
+mysql_conn.close()
